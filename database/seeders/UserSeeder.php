bf738bee2c7b99d56bc689d5ee1699607e6670ed
Ryuzu <ryuzugfx@hotmail.com>
2023-09-07 01:40:25 -0500

    update: using constants variables for user seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,21 +7,24 @@ use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
+    const USERS_COUNT = 10000;
+    const CHUNK_SIZE = 1000;
+    
     public function run(): void
     {
         $data = [];
 
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < static::USERS_COUNT; $i++) {
             $data[] = [
                 'name' => fake()->name,
                 'email' => fake()->unique()->safeEmail,
-                'email_verified_at' => fake()->dateTime,
+                'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
                 'remember_token' => \Str::random(10),
             ];
         }
 
-        $chunks = array_chunk($data, 1000);
+        $chunks = array_chunk($data, static::CHUNK_SIZE);
 
         foreach ($chunks as $chunk) {
             DB::table('users')->insert($chunk);
